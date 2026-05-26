a140c06dbfdb3ce967328e8e7ea75472e008f670
Bagasss08 <bwidyastama@gmail.com>
2026-05-27 00:38:12 +0700

    revisi terus

--- resources/views/kelulusan/index.blade.php
+++ resources/views/kelulusan/index.blade.php
@@ -6,53 +6,55 @@
 </head>
 <body>
 
-    <h1>Cek Kelulusan</h1>
+<h1>Cek Kelulusan</h1>
 
-    @if(session('error'))
-        <p>{{ session('error') }}</p>
-    @endif
+@if(session('error'))
+    <p>{{ session('error') }}</p>
+@endif
 
-    @if ($errors->any())
-        <p>{{ $errors->first() }}</p>
-    @endif
+@if ($errors->any())
+    <p>{{ $errors->first() }}</p>
+@endif
 
-    <form action="{{ route('cek.kelulusan') }}" method="POST">
+<form action="{{ route('cek.kelulusan') }}" method="POST">
 
-        @csrf
+    @csrf
 
-        <div>
-            <label>NISN</label>
-            <br>
-
-            <input
-                type="number"
-                name="nisn"
-                value="{{ old('nisn') }}"
-                required
-            >
-        </div>
-
+    <div>
+        <label>NISN</label>
         <br>
 
-        <div>
-            <label>Tanggal Lahir</label>
-            <br>
+        <input
+            type="text"
+            name="nisn"
+            value="{{ old('nisn') }}"
+            inputmode="numeric"
+            pattern="[0-9]*"
+            required
+        >
+    </div>
 
-            <input
-                type="date"
-                name="tanggal_lahir"
-                value="{{ old('tanggal_lahir') }}"
-                required
-            >
-        </div>
+    <br>
 
+    <div>
+        <label>Tanggal Lahir</label>
         <br>
 
-        <button type="submit">
-            Cek Kelulusan
-        </button>
+        <input
+            type="date"
+            name="tanggal_lahir"
+            value="{{ old('tanggal_lahir') }}"
+            required
+        >
+    </div>
 
-    </form>
+    <br>
+
+    <button type="submit">
+        Cek Kelulusan
+    </button>
+
+</form>
 
 </body>
 </html>
