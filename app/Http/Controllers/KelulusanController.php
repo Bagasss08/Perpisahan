25c6313d98ae8a8f75a45f0ff9175ab214a3488c
Bagasss08 <bwidyastama@gmail.com>
2026-05-27 00:28:36 +0700

    update cek

--- app/Http/Controllers/KelulusanController.php
+++ app/Http/Controllers/KelulusanController.php
@@ -13,27 +13,26 @@ class KelulusanController extends Controller
     }
 
     public function cek(Request $request)
-{
-    // Jika akses GET langsung ke /cek
-    if ($request->isMethod('get')) {
-        return redirect('/');
+    {
+        if ($request->isMethod('get')) {
+            return redirect('/');
+        }
+
+        $request->validate([
+            'nisn' => ['required', 'digits:10'],
+            'tanggal_lahir' => ['required', 'date'],
+        ]);
+
+        $siswa = Siswa::where('nisn', $request->nisn)
+            ->whereDate('tanggal_lahir', $request->tanggal_lahir)
+            ->first();
+
+        if (!$siswa) {
+            return back()->withErrors([
+                'nisn' => 'Data siswa tidak ditemukan.'
+            ])->withInput();
+        }
+
+        return view('kelulusan.hasil', compact('siswa'));
     }
-
-    $request->validate([
-        'nisn'          => ['required', 'digits:10'],
-        'tanggal_lahir' => ['required', 'date'],
-    ]);
-
-    $siswa = Siswa::where('nisn', $request->nisn)
-        ->whereDate('tanggal_lahir', $request->tanggal_lahir)
-        ->first();
-
-    if (!$siswa) {
-        return back()->withErrors([
-            'nisn' => 'Data siswa tidak ditemukan.'
-        ])->withInput();
-    }
-
-    return view('kelulusan.hasil', compact('siswa'));
-}
 }
