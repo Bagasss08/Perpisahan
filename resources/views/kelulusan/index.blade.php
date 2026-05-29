ef1d63688030acf6706b5ea6d15b04fa79cc5255
Bagasss08 <bwidyastama@gmail.com>
2026-05-29 23:56:13 +0700

    cek revisi

--- resources/views/kelulusan/index.blade.php
+++ resources/views/kelulusan/index.blade.php
@@ -1,41 +1,51 @@
-<form method="POST" action="{{ url('/cek') }}">
-
-    @csrf
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cek Kelulusan</title>
+</head>
+<body>
 
     @if(session('error'))
-    <div style="color: red; margin-bottom: 10px;">
-        {{ session('error') }}
-    </div>
-@endif
+        <div style="color: red; margin-bottom: 10px;">
+            {{ session('error') }}
+        </div>
+    @endif
 
-    <div>
-        <label>NISN</label>
+    <form method="POST" action="{{ route('cek.kelulusan') }}">
+
+        @csrf
+
+        <div>
+            <label>NISN</label>
+            <br>
+            <input
+                type="text"
+                name="nisn"
+                inputmode="numeric"
+                autocomplete="off"
+                required
+            >
+        </div>
+
         <br>
 
-        <input
-            type="text"
-            name="nisn"
-            inputmode="numeric"
-            autocomplete="off"
-            required
-        >
-    </div>
+        <div>
+            <label>Tanggal Lahir</label>
+            <br>
+            <input
+                type="date"
+                name="tanggal_lahir"
+                required
+            >
+        </div>
 
-    <br>
-
-    <div>
-        <label>Tanggal Lahir</label>
         <br>
 
-        <input
-            type="date"
-            name="tanggal_lahir"
-            required
-        >
-    </div>
+        <button type="submit">Cek Kelulusan</button>
 
-    <br>
+    </form>
 
-    <input type="submit" value="Cek Kelulusan">
-
-</form>
+</body>
+</html>
